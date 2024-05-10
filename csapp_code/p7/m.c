23875b61a581fc55b865b1d46cfd7f2d3328878c
zhoukang <1123180416@qq.com>
2024-05-11 01:12:49 +0800

    symtab

---
+++ csapp_code/p7/m.c
@@ -0,0 +1,11 @@
+void swap();
+
+int buf[2] = {1,2};
+
+int main()
+{
+	swap();
+	return 0;
+}
+
+
---
+++ csapp_code/p7/mode.c
@@ -0,0 +1,11 @@
+int f()
+{
+	static int x = 0;
+	return x;
+}
+
+int g()
+{
+	static int x = 1;
+	return x;
+}
